94b81f567a1c2c530fde641de106b7f44940f762
vijaykumar930 <52034199+vijaykumar930@users.noreply.github.com>
2022-03-01 22:12:47 +0530

    Create Replace all 0s with 5.java

---
+++ Replace all 0s with 5.java
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+/* You are given an integer N. You need to convert all zeroes of N to 5.
+
+Example 1:
+
+Input:
+N = 1004
+Output: 1554
+Explanation: There are two zeroes in 1004
+on replacing all zeroes with "5", the new
+number will be "1554".
+Example 2:
+
+Input:
+N = 121
+Output: 121
+Explanation: Since there are no zeroes in
+"121", the number remains as "121".
+Your Task:
+Your task is to complete the function convertFive() which takes an integer N as an argument and replaces all zeros in the number N with 5. Your function should return the converted number.
+
+Expected Time Complexity: O(K) where K is the number of digits in N
+Expected Auxiliary Space: O(1)*/
+
+import java.util.Scanner;
+import java.lang.Math;
+
+class Convert_To_Five {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0) {
+            int N = sc.nextInt();
+            System.out.println(new GfG().convertfive(N));
+            T--;
+        }
+    }
+}// } Driver Code Ends
+
+
+class GfG {
+    int convertfive(int num) {
+        // Your code here
+        return Integer.parseInt(Integer.toString(num).replace('0','5'));
+    }
+}
