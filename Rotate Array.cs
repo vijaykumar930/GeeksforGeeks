3d536d64c408fab4fd1b3725e3bddaafb17ca589
vijaykumar930 <52034199+vijaykumar930@users.noreply.github.com>
2022-03-01 22:54:01 +0530

    Update Rotate Array.cs

--- Rotate Array.cs
+++ Rotate Array.cs
@@ -1,5 +1,35 @@
 // { Driver Code Starts
 //Initial Template for C#
+/* Given an unsorted array arr[] of size N. Rotate the array to the left (counter-clockwise direction) by D steps, where D is a positive integer. 
+
+ 
+
+Example 1:
+
+Input:
+N = 5, D = 2
+arr[] = {1,2,3,4,5}
+Output: 3 4 5 1 2
+Explanation: 1 2 3 4 5  when rotated
+by 2 elements, it becomes 3 4 5 1 2.
+Example 2:
+
+Input:
+N = 10, D = 3
+arr[] = {2,4,6,8,10,12,14,16,18,20}
+Output: 8 10 12 14 16 18 20 2 4 6
+Explanation: 2 4 6 8 10 12 14 16 18 20 
+when rotated by 3 elements, it becomes 
+8 10 12 14 16 18 20 2 4 6.
+ 
+
+Your Task:
+Complete the function rotateArr() which takes the array, D and N as input parameters and rotates the array by D elements. The array must be modified in-place without using extra space. 
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)*/
 
 using System;
 using System.Collections.Generic;
@@ -48,37 +78,6 @@ namespace DriverCode
     
  // } Driver Code Ends
 //User function Template for C#
-/* Given an unsorted array arr[] of size N. Rotate the array to the left (counter-clockwise direction) by D steps, where D is a positive integer. 
-
- 
-
-Example 1:
-
-Input:
-N = 5, D = 2
-arr[] = {1,2,3,4,5}
-Output: 3 4 5 1 2
-Explanation: 1 2 3 4 5  when rotated
-by 2 elements, it becomes 3 4 5 1 2.
-Example 2:
-
-Input:
-N = 10, D = 3
-arr[] = {2,4,6,8,10,12,14,16,18,20}
-Output: 8 10 12 14 16 18 20 2 4 6
-Explanation: 2 4 6 8 10 12 14 16 18 20 
-when rotated by 3 elements, it becomes 
-8 10 12 14 16 18 20 2 4 6.
- 
-
-Your Task:
-Complete the function rotateArr() which takes the array, D and N as input parameters and rotates the array by D elements. The array must be modified in-place without using extra space. 
-
- 
-
-Expected Time Complexity: O(N)
-Expected Auxiliary Space: O(1)
-*/
 
     class Solution
     {
@@ -86,26 +85,37 @@ Expected Auxiliary Space: O(1)
         public void rotateArr(int[] arr, int n, int d)
         {
             //Your code here
-            int i=0;
-            int j=0;
-            int temp = 0;
-            for(i=d;i<arr.Length;i++)
+            int[] temp = new int[d];
+            int tempd=d;
+            for(int i=0;i<d;i++)
             {
-                temp = arr[d];
-                Console.WriteLine("temp = " + temp);
-                if(i+1 == arr.Length)
-                {
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                    j++;
-                }
-                else 
-                {
-                    Console.WriteLine("arr["+i+"] = arr["+(i+1)+"] ");
-                    arr[i] = arr[i+1];
-                }
-                
+                temp[i] = arr[i];
             }
+            /*Console.WriteLine("Temp array:");
+            for(int i=0;i<d;i++)
+            {
+                Console.WriteLine(" " + temp[i]);
+            }*/
+            for(int i=d;i<n;i++)
+            {
+                arr[i-d] = arr[i];
+            }
+            /*Console.WriteLine("After Left shift:");
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine(" " + arr[i]);
+            }*/
+            for(int i=0;i<d;i++)
+            {
+                arr[n-tempd]=temp[i];
+                //Console.WriteLine("arr["+n+"-"+tempd+"] = " + arr[n-tempd]);
+                tempd--;
+            }
+            /*Console.WriteLine("After appending temp array:");
+            for(int i=0;i<n;i++)
+            {
+                Console.WriteLine(" " + arr[i]);
+            }*/
         }
     }
 
