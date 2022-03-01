05b39bd2b0f906c4d9d0d5c2b6c4c10c2157dbb7
vijaykumar930 <52034199+vijaykumar930@users.noreply.github.com>
2022-03-01 22:18:08 +0530

    Create Missing number in array.py

---
+++ Missing number in array.py
@@ -0,0 +1,52 @@
+#User function Template for python3
+'''  Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
+
+Example 1:
+
+Input:
+N = 5
+A[] = {1,2,3,5}
+Output: 4
+Example 2:
+
+Input:
+N = 10
+A[] = {6,1,2,8,3,4,7,10,5}
+Output: 9
+
+Your Task :
+You don't need to read input or print anything. Complete the function MissingNumber() that takes array and N as input  parameters and returns the value of the missing number.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+'''
+
+class Solution:
+    def MissingNumber(self,array,n):
+        # code here
+        array.sort();
+        counter=0;
+        for i in range(0,n):
+            counter+=1;
+            if(i == len(array)):
+                return counter;
+            if(counter == array[i]):
+                continue;
+            else:
+                return counter;
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+
+
+
+t=int(input())
+for _ in range(0,t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    s=Solution().MissingNumber(a,n)
+    print(s)
+# } Driver Code Ends
