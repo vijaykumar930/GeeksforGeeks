be6c60394849c2033fe5d1967d9bd0aae95010f6
vijaykumar930 <52034199+vijaykumar930@users.noreply.github.com>
2022-03-01 22:16:36 +0530

    Create Trailing zeroes in factorial.py

---
+++ Trailing zeroes in factorial.py
@@ -0,0 +1,48 @@
+#User function Template for python3
+'''  For an integer N find the number of trailing zeroes in N!.
+
+Example 1:
+
+Input:
+N = 5
+Output:
+1
+Explanation:
+5! = 120 so the number of trailing zero is 1.
+Example 2:
+
+Input:
+N = 4
+Output:
+0
+Explanation:
+4! = 24 so the number of trailing zero is 0.
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function trailingZeroes() which take an integer N as an input parameter and returns the count of trailing zeroes in the N!.
+
+Expected Time Complexity: O(logN)
+Expected Auxiliary Space: O(1)'''
+
+import math;
+
+class Solution:
+    def trailingZeroes(self, N):
+        #code here 
+        counter=0;
+        while N>=5:
+            N = math.floor(N/5);
+            counter+=N;
+        return counter;
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        N = int(input()) 
+        ob = Solution()
+        ans = ob.trailingZeroes(N)
+        print(ans)
+# } Driver Code Ends
